a598a4ae679f18249c8dca2928880fc35cf5c919
Joseph <napolitanjoe@gmail.com>
2021-11-17 00:55:53 -0500

    Version 0.6, check Readme.md, mostly functional

--- Readme.md
+++ Readme.md
@@ -3,6 +3,13 @@ Calculator
 This is a project for The Odin Project's foundation course. My intention for the project is to refine and demonstrate my knowledge of HTML, CSS, and Javascript.
 
 -----------
+Version 0.6
+-----------
+
+Numerous fixes have been made adhering to TOP Gotcha #1 listed in the assignment. The calculator can now string together multiple operations. Also, the functions return/display appropriate results. The delete button now works. A bug of note is entering an operation after pressing the equal sign. Otherwise, the calculator is relatively functional.
+
+
+-----------
 Version 0.5
 -----------
 
--- script.js
+++ script.js
@@ -1,65 +1,84 @@
 const equal = document.getElementById("equal");
 const clear = document.getElementById("clear");
+const erase = document.getElementById("delete");
 let display = document.getElementById("display");
 let numButton = document.querySelectorAll('[data-number]');
 let opButton = document.querySelectorAll('[data-op]');
 let firstNum = "";
 let secondNum = "";
+let total = null;
 let operator = undefined;
 
 
 //Event Listeners for all buttons
-equal.addEventListener("click", ()=> operate(firstNum,operator,parseInt(display.innerText.replace(firstNum,"").replace(operator,""))));
-clear.addEventListener("click", ()=> hardReset());
+equal.addEventListener("click", ()=> 
+    operate(total,operator,Number(display.innerText.replace(firstNum,"").replace(operator,""))));
+
+clear.addEventListener("click", ()=> 
+    hardReset());
+
+erase.addEventListener("click", ()=> 
+    display.textContent = display.textContent.slice(0,-1));
 
 numButton.forEach((button) => button.addEventListener('click', () =>
-display.innerText += button.textContent));
+    inputNum(button.textContent)));
 
 opButton.forEach((button) => button.addEventListener('click', () => 
-store(button.textContent)));
+    store(button.textContent)));
 
 
 //Store num and Operator
-function store(displayed) {
-    if (operator === undefined) {
-        operator = displayed;
-        firstNum = parseInt(display.innerText);
-        display.innerText = firstNum + operator;
+function store(chosen) {
+    if (total === null) {
+        operator = chosen;
+        total = Number(display.innerText);
+        display.innerText = total + operator;
     }
     else {
-        secondNum = parseInt(display.innerText.replace(firstNum,"").replace(operator,""));
-        operate(firstNum,operator,secondNum);
+        operator = chosen;
+        secondNum = Number(display.innerText);
+        display.innerText = operate(total,operator,secondNum)
     }
+
+}
+
+function inputNum(entry) {
+    if (total === null)
+        display.innerText += entry;
+    else
+        display.innerText = entry;
 }
 
 
-function clearScreen() {
-    display.innerText = "";
-}
-
+//Clear
 function hardReset() {
 firstNum = 0;
 secondNum = 0;
+total = null;
 operator = undefined;
-display.innerText = " ";
+display.innerText = "";
 }
 
-//Operators 'N Stuff
+//Operators 'N Math Functions
 function add(num1,num2) {
     console.log(num1+num2);
-    return (num1+num2);
+    total = (num1+num2);
+    return total;
 }
 function sub(num1,num2) {
     console.log(num1-num2);
-    return (num1-num2);
+    total = (num1-num2);
+    return total;
 }
 function mul(num1,num2) {
     console.log(num1*num2);
-    return (num1*num2);
+    total = (num1*num2);
+    return total;
 }
 function dvd(num1,num2) {
     console.log(num1/num2);
-    return (num1/num2);
+    total = (num1/num2);
+    return total;
 }
 function operate(a, oper, b) {
     console.log(a);
@@ -69,17 +88,14 @@ function operate(a, oper, b) {
         case "+" :
             display.innerText = add(a,b);
             return add(a,b);
-            break;
         case "-" :
             display.innerText = sub(a,b);
             return sub(a,b);
-            break;
         case "X" :
             display.innerText = mul(a,b);
             return mul(a,b);
-            break;
         case "/" :
             display.innerText = dvd(a,b);
-            break;
+            return mul(a,b);   
     }
 }
